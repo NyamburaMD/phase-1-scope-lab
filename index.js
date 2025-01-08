dce8b1ab6e076b8d97d40cdad7868df9b549e23a
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-01-08 15:33:26 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+// Write your solution in this file
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'; //global because we didn't use var, let or const
+}
+function overwriteBestCustomer () {
+    bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'someone';
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'anyone else';
+}
